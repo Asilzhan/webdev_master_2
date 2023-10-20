f5603773382b6d79359dbac37e6ab68107912358
Auzbayeva Assel Maratovna <asel_shu.kz@mail.ru>
2023-10-20 19:30:34 +0600

    Add CabinetPage

---
+++ cuvi/webclient/src/features/cabinets/pages/CabinetNotFoundPage.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="not-found-container">
+    <h1>Cabinet Not Found</h1>
+    <p>Sorry, we couldn't find the cabinet you're looking for.</p>
+    <router-link to="/cabinets">Go back to cabinets</router-link>
+  </div>
+</template>
+
